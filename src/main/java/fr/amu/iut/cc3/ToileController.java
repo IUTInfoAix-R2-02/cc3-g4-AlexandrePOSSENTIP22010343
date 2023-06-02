4f7fd67c278bdba20708896e522da2b5902d568e
p22010343 <alexandre.possenti@etu.univ-amu.fr>
2023-06-02 15:06:42 +0200

    tracé points et message d'erreur

--- src/main/java/fr/amu/iut/cc3/ToileController.java
+++ src/main/java/fr/amu/iut/cc3/ToileController.java
@@ -10,13 +10,18 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Point2D;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 
 import java.net.URL;
@@ -30,6 +35,80 @@ public class ToileController implements Initializable {
     private static int angleEnDegre = 60;
     private static int angleDepart = 90;
     private static int noteMaximale = 20;
+
+    @FXML
+    TextField competence1;
+    @FXML
+    TextField competence2;
+    @FXML
+    TextField competence3;
+    @FXML
+    TextField competence4;
+    @FXML
+    TextField competence5;
+    @FXML
+    TextField competence6;
+    @FXML
+    Pane pane;
+    @FXML
+    Label erreur;
+    @FXML
+    Label erreurName;
+    public static boolean isNumeric(String s) {
+        try {
+            Double.parseDouble(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    @FXML
+    private void addPoint(ActionEvent event) {
+        TextField field = (TextField)event.getSource();
+        if (!isNumeric(field.getText()) || field.getText().isBlank() || field.getText().isEmpty() || Double.parseDouble(field.getText())>20 || Double.parseDouble(field.getText())<0){
+            erreur.setText("Erreur de saisie :");
+            erreur.setTextFill(Color.RED);
+            erreurName.setTextFill(Color.RED);
+            if(!isNumeric(field.getText()))
+                erreurName.setText("les valeurs doivent etre decimales");
+            else
+                erreurName.setText("les valeurs doivent etre entre 0 et 20");
+            return;
+        }
+
+        Double note = Double.parseDouble(field.getText());
+        System.out.println(field.getText() + field.getId());
+        int axe = 0;
+        switch (field.getId()){
+            case "competence1":
+                axe = 1;
+                break;
+            case "competence2":
+                axe = 2;
+                break;
+            case "competence3":
+                axe = 3;
+                break;
+            case "competence4":
+                axe = 4;
+                break;
+            case "competence5":
+                axe = 5;
+                break;
+            case "competence6":
+                axe = 6;
+                break;
+        }
+        Circle circle = new Circle();
+        circle.setCenterX(getXRadarChart(note,axe));
+        circle.setCenterY(getYRadarChart(note,axe));
+        circle.setRadius(5);
+        circle.setStyle("-fx-fill-color: Black");
+        pane.getChildren().add(circle);
+
+
+    }
 
 
     @Override
